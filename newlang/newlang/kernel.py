896652c9e2581f9d349f8677419c45804a1e8d71
Alexander Kalistratov <alexander.kalistratov@intel.com>
2024-03-05 22:30:08 +0100

    Add _masked_array wrapper, atomic_ref and few fixes

--- newlang/newlang/kernel.py
+++ newlang/newlang/kernel.py
@@ -1,19 +1,22 @@
-
 from collections.abc import Iterable
 from functools import partial
 from greenlet import greenlet
 from itertools import product
 from numpy import ma
 from sympy import Symbol
+from math import prod
 import inspect
 import numpy as np
 import types
 import typing
+import functools
+import operator
 
 from .indexing import sym
 
-DEF_GROUP_SHAPE = (64,1,1)
+DEF_GROUP_SHAPE = (64, 1, 1)
 DEF_SUBGROUP_SIZE = 16
+
 
 class TunableParam:
     def __init__(self, sym, default, vals, strategy=None):
@@ -21,6 +24,7 @@ class TunableParam:
         self.default = default
         self.vals = vals
         self.strategy = strategy
+
 
 def _get_default_tunables(tunables):
     if isinstance(tunables, TunableParam):
@@ -31,13 +35,6 @@ def _get_default_tunables(tunables):
 
     assert False, "Unsupported tunables"
 
-def copy_func(f):
-    g = types.FunctionType(f.__code__, f.__globals__, name=f.__name__,
-                           argdefs=f.__defaults__,
-                           closure=f.__closure__)
-    g = functools.update_wrapper(g, f)
-    g.__kwdefaults__ = f.__kwdefaults__
-    return g
 
 def resolve_symbols(func, symbols):
     old_closure = func.__closure__
@@ -45,6 +42,7 @@ def resolve_symbols(func, symbols):
 
     if old_closure is not None:
         cell_cls = type(old_closure[0])
+
         def resolve_cell(cell):
             if isinstance(cell.cell_contents, Symbol):
                 if cell.cell_contents in symbols.keys():
@@ -63,15 +61,22 @@ def resolve_symbols(func, symbols):
 
     new_globals = {key: resolve_global(val) for key, val in func.__globals__.items()}
 
-    g = types.FunctionType(func.__code__, new_globals, name=func.__name__,
-                           argdefs=func.__defaults__,
-                           closure=new_closure)
+    g = types.FunctionType(
+        func.__code__,
+        new_globals,
+        name=func.__name__,
+        argdefs=func.__defaults__,
+        closure=new_closure,
+    )
     g = functools.update_wrapper(g, func)
     g.__kwdefaults__ = func.__kwdefaults__
     return g
 
+
 class _kernel:
-    def __init__(self, func, work_shape, group_shape, subgroup_size, literals, tunables, tuned={}):
+    def __init__(
+        self, func, work_shape, group_shape, subgroup_size, literals, tunables, tuned={}
+    ):
         self.handler = _visit_func_annotations(func)
         self.orig_func = func
         self.work_shape = work_shape
@@ -104,17 +109,146 @@ class _kernel:
             resolved_func(cg, *args, **kwargs)
 
     def parametrize(self, tuned):
-        return _kernel(self.orig_func, self.work_shape, self.group_shape, self.subgroup_size, self.literals, self.tunables, self.tuned | tuned)
+        return _kernel(
+            self.orig_func,
+            self.work_shape,
+            self.group_shape,
+            self.subgroup_size,
+            self.literals,
+            self.tunables,
+            self.tuned | tuned,
+        )
+
+
+def _not(val):
+    if isinstance(val, (bool, int)):
+        return val == 0
+
+    if isinstance(val, np.ndarray):
+        return ~val
+
+    if isinstance(val, (tuple, list)):
+        return type(val)([_not(v) for v in val])
+
+    assert False
+
+
+class _masked_array:
+    def __init__(self, data, mask=True):
+        if isinstance(data, ma.masked_array):
+            assert mask
+            self.__ma = data
+        else:
+            self.__ma = ma.masked_array(data, mask=_not(mask))
+
+    def __set_mask(self, key, value):
+        self.__ma.mask[key] = _not(value)
+
+    @property
+    def mask(self):
+        return _not(self.__ma.mask)
+
+    @property
+    def data(self):
+        return self.__ma.data
+
+    def __setitem__(self, key, value):
+        if isinstance(value, _masked_array):
+            self.data[key] = np.where(value.mask, value.data, self.data[key])
+            self.__set_mask(key, self.mask | value.mask)
+        else:
+            self.data[key] = value
+            self.__set_mask(key, True)
+
+    def __getitem__(self, key):
+        return self.__ma[key]
+
+    def _bin_op_implementation(self, other, op):
+        if isinstance(other, _masked_array):
+            return _masked_array(op(self.__ma, other.__ma))
+        else:
+            return _masked_array(op(self.__ma, other))
+
+    def _bin_iop_implementation(self, other, op):
+        self[:] = op(self, other)
+
+        return self
+
+    def _un_op_implemenation(self, op):
+        return _masked_array(op(self.__ma))
+
+    def __getattr__(self, name):
+        return getattr(self.__ma, name)
+
+
+def set_arithm_methods():
+    bin_op_list = [
+        operator.add,
+        operator.sub,
+        operator.mul,
+        operator.matmul,
+        operator.truediv,
+        operator.floordiv,
+        operator.pow,
+        operator.mod,
+        operator.lt,
+        operator.le,
+        operator.eq,
+        operator.ne,
+        operator.ge,
+        operator.gt,
+        operator.and_,
+        operator.or_,
+        operator.xor,
+        operator.rshift,
+        operator.lshift,
+    ]
+
+    un_op_list = [operator.abs, operator.neg, operator.not_, operator.inv]
+
+    def _name(op):
+        return op.__name__.replace("_", "")
+
+    def set_bin_attr(op):
+        setattr(
+            _masked_array,
+            f"__{_name(op)}__",
+            lambda self, other: self._bin_op_implementation(other, op),
+        )
+        setattr(
+            _masked_array,
+            f"__i{_name(op)}__",
+            lambda self, other: self._bin_iop_implementation(other, op),
+        )
+
+    def set_un_attr(op):
+        setattr(
+            _masked_array,
+            f"__{_name(op)}__",
+            lambda self: self._un_op_implemenation(op),
+        )
+
+    for op in bin_op_list:
+        set_bin_attr(op)
+
+    for op in un_op_list:
+        set_un_attr(op)
+
+
+set_arithm_methods()
 
 
 def _get_uninit_value(dtype):
     return 0
 
+
 def _divup(a, b):
     return (a + b - 1) // b
 
+
 def _get_num_groups(work_shape, group_shape):
     return tuple(_divup(a, b) for a, b in zip(work_shape, group_shape))
+
 
 class CurrentSubGroup:
     def __init__(self, size, subgroup_id):
@@ -144,20 +278,38 @@ class CurrentGroup:
     def __init__(self, group_shape, subgroup_size):
         self._group_shape = group_shape
         self._subgroup_size = subgroup_size
-        self._subgroup_ranges = (range(group_shape[0]), range(group_shape[1]), range(_divup(group_shape[2], subgroup_size)))
-        self._workitem_ranges = (range(group_shape[0]), range(group_shape[1]), range(group_shape[2]))
+        self._subgroup_ranges = (
+            range(group_shape[0]),
+            range(group_shape[1]),
+            range(_divup(group_shape[2], subgroup_size)),
+        )
+        self._workitem_ranges = (
+            range(group_shape[0]),
+            range(group_shape[1]),
+            range(group_shape[2]),
+        )
         self._group_id = None
         self._tasks = []
         self._current_task = 0
 
-    def group_id(self):
+    @property
+    def id(self):
         return self._group_id
 
-    def group_shape(self):
+    @property
+    def shape(self):
         return self._group_shape
 
+    @property
+    def size(self):
+        if isinstance(self._group_shape, int):
+            return self._group_shape
+
+        return prod(self._group_shape)
+
+    @property
     def work_offset(self):
-        return tuple(a * b for a,b in zip(self._group_shape, self._group_id))
+        return tuple(a * b for a, b in zip(self._group_shape, self._group_id))
 
     def load(self, array, shape):
         if not isinstance(shape, Iterable):
@@ -165,14 +317,15 @@ class CurrentGroup:
 
         dtype = array.dtype
         init = _get_uninit_value(dtype)
-        res = ma.masked_array(np.full(shape, fill_value=init, dtype=dtype), mask=True)
+        res = _masked_array(np.full(shape, fill_value=init, dtype=dtype), mask=False)
         s = tuple(slice(0, min(s1, s2)) for s1, s2 in zip(array.shape, shape))
+
         res[s] = array[s]
         return res
 
     def store(self, dst, src):
         s = tuple(slice(0, min(s1, s2)) for s1, s2 in zip(src.shape, dst.shape))
-        dst[s] = np.where(src.mask[s], dst[s], src[s])
+        dst[s] = np.where(src.mask[s], src[s], dst[s])
 
     def empty(self, shape, dtype):
         return self._alloc_impl(shape, dtype, _get_uninit_value(dtype))
@@ -211,38 +364,39 @@ class CurrentGroup:
 
         def _func():
             tasks = self._tasks
-            assert(len(tasks) == 0)
-            assert(self._current_task == 0)
+            assert len(tasks) == 0
+            assert self._current_task == 0
             for sgid in product(*self._subgroup_ranges):
-                tasks.append(greenlet(partial(_body_wrapper,sgid)))
+                tasks.append(greenlet(partial(_body_wrapper, sgid)))
 
             for t in tasks:
                 t.switch()
 
             self._current_task = 0
             tasks.clear()
-
 
         return _func
 
     def workitems(self, func):
         def _body_wrapper(lid):
-            gid = tuple(gi * gs + li for gi, gs, li in zip(self._group_id, self._group_shape, lid))
+            gid = tuple(
+                gi * gs + li
+                for gi, gs, li in zip(self._group_id, self._group_shape, lid)
+            )
             func(CurrentWorkitem(lid, gid))
 
         def _func():
             tasks = self._tasks
-            assert(len(tasks) == 0)
-            assert(self._current_task == 0)
+            assert len(tasks) == 0
+            assert self._current_task == 0
             for lid in product(*self._workitem_ranges):
-                tasks.append(greenlet(partial(_body_wrapper,lid)))
+                tasks.append(greenlet(partial(_body_wrapper, lid)))
 
             for t in tasks:
                 t.switch()
 
             self._current_task = 0
             tasks.clear()
-
 
         return _func
 
@@ -260,9 +414,10 @@ class CurrentGroup:
         tasks[next_task].switch()
 
     def _alloc_impl(self, shape, dtype, init):
-        return ma.masked_array(np.full(shape, fill_value=init, dtype=dtype), mask=False)
+        return _masked_array(np.full(shape, fill_value=init, dtype=dtype), mask=True)
 
-class Buffer(typing.Generic[typing.ParamSpec('Args')]):
+
+class Buffer(typing.Generic[typing.ParamSpec("Args")]):
     pass
 
 
@@ -281,17 +436,20 @@ def _add_sub(subs, sym, val):
     else:
         subs[sym] = val
 
+
 def _visit_arg_annotation(idx, ann, prev_handler):
     if ann in (CurrentGroup, CurrentSubGroup, CurrentWorkitem):
         # nothing
         return
 
     if isinstance(ann, Symbol):
+
         def handler(subs, args):
             val = args[idx]
             _add_sub(subs, ann, val)
 
     elif typing.get_origin(ann) == tuple:
+
         def handler(subs, args):
             val = args[idx]
             assert isinstance(val, Iterable)
@@ -304,6 +462,7 @@ def _visit_arg_annotation(idx, ann, prev_handler):
                 _add_sub(subs, s, v)
 
     elif typing.get_origin(ann) == Buffer:
+
         def handler(subs, args):
             val = args[idx]
             ann_args = typing.get_args(ann)[0]
@@ -318,6 +477,7 @@ def _visit_arg_annotation(idx, ann, prev_handler):
         assert False, f"Unsupported annotation: {type(ann)} {ann}"
 
     if prev_handler:
+
         def chained(subs, args):
             prev_handler(subs, args)
             handler(subs, args)
@@ -339,21 +499,35 @@ def _visit_func_annotations(func):
 
     return handler
 
+
 def _handle_dim(src, subs):
     if isinstance(src, Iterable):
         return tuple(_handle_dim(v, subs) for v in src)
 
-    if isinstance(src, Symbol):
+    if hasattr(src, "subs"):
         src = src.subs(subs)
 
     return int(src)
 
 
-def kernel(work_shape, group_shape=DEF_GROUP_SHAPE, subgroup_size=DEF_SUBGROUP_SIZE, literals=(), tunables=()):
-    assert isinstance(subgroup_size, int) or subgroup_size in literals, "Subgroup size must be const or literal"
+def kernel(
+    work_shape,
+    group_shape=DEF_GROUP_SHAPE,
+    subgroup_size=DEF_SUBGROUP_SIZE,
+    literals=(),
+    tunables=(),
+):
+    assert (
+        isinstance(subgroup_size, int) or subgroup_size in literals
+    ), "Subgroup size must be const or literal"
     work_shape = _get_dims(work_shape)
     group_shape = _get_dims(group_shape)
+
     def _kernel_impl(func):
         return _kernel(func, work_shape, group_shape, subgroup_size, literals, tunables)
 
     return _kernel_impl
+
+
+def atomic_ref(a):
+    return a
