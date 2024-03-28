765f130c258e9b8056b76f6e04e363d172873743
Ivan Butygin <ivan.butygin@gmail.com>
2024-03-28 20:48:10 +0100

    Composite pass

---
+++ newlang/hckernel/mlir/lib/Transforms/CompositePass.cpp
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "hc/Transforms/Passes.hpp"
+
+#include "mlir/Pass/PassManager.h"
+#include <mlir/Pass/Pass.h>
+
+namespace {
+struct CompositePass final
+    : public mlir::PassWrapper<CompositePass, mlir::OperationPass<void>> {
+  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(CompositePass)
+
+  CompositePass(std::string name_,
+                std::function<void(mlir::OpPassManager &)> func,
+                unsigned maxIterations)
+      : name(std::move(name_)), populateFunc(std::move(func)),
+        maxIters(maxIterations) {}
+
+  void getDependentDialects(mlir::DialectRegistry &registry) const override {
+    mlir::OpPassManager pm;
+    populateFunc(pm);
+    pm.getDependentDialects(registry);
+  }
+
+  void runOnOperation() override {
+    assert(populateFunc);
+    auto op = getOperation();
+    mlir::OperationFingerPrint fp(op);
+
+    unsigned currentIter = 0;
+    mlir::OpPassManager dynamicPM(op->getName());
+    populateFunc(dynamicPM);
+    while (true) {
+      if (mlir::failed(runPipeline(dynamicPM, op)))
+        return signalPassFailure();
+
+      if (currentIter++ >= maxIters) {
+        op->emitWarning("Composite pass \"" + llvm::Twine(name) +
+                        "\"+ didn't converge in " + llvm::Twine(maxIters) +
+                        " iterations");
+        break;
+      }
+
+      mlir::OperationFingerPrint newFp(op);
+      if (newFp == fp)
+        break;
+
+      fp = newFp;
+    }
+  }
+
+protected:
+  llvm::StringRef getName() const override {
+    assert(!name.empty());
+    return name;
+  }
+
+private:
+  std::string name;
+  std::function<void(mlir::OpPassManager &)> populateFunc;
+  unsigned maxIters;
+};
+} // namespace
+
+std::unique_ptr<mlir::Pass>
+hc::createCompositePass(std::string name,
+                        std::function<void(mlir::OpPassManager &)> populateFunc,
+                        unsigned maxIterations) {
+  assert(!name.empty());
+  assert(populateFunc);
+  return std::make_unique<CompositePass>(
+      std::move(name), std::move(populateFunc), maxIterations);
+}
