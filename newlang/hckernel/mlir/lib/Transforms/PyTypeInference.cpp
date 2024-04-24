33565c7adb4d2cea1db4576e8712bfbe1cea03bd
Ivan Butygin <ivan.butygin@gmail.com>
2024-04-24 04:14:43 +0200

    Use a proper dataflow analysis for type inference

--- newlang/hckernel/mlir/lib/Transforms/PyTypeInference.cpp
+++ newlang/hckernel/mlir/lib/Transforms/PyTypeInference.cpp
@@ -9,7 +9,9 @@
 
 #include <queue>
 
-#include <mlir/Dialect/ControlFlow/IR/ControlFlowOps.h>
+#include <mlir/Analysis/DataFlow/ConstantPropagationAnalysis.h>
+#include <mlir/Analysis/DataFlow/DeadCodeAnalysis.h>
+#include <mlir/Analysis/DataFlow/SparseAnalysis.h>
 #include <mlir/IR/Builders.h>
 #include <mlir/IR/PatternMatch.h>
 
@@ -182,6 +184,166 @@ private:
       resolversMap;
 };
 
+struct TypeValue {
+  TypeValue() = default;
+  TypeValue(mlir::Type t) : type(t) {}
+  TypeValue(std::string desc) : errDesc(std::move(desc)) {
+    assert(!errDesc.empty());
+  }
+
+  static TypeValue getInvalid(mlir::Operation *op) {
+    std::string str;
+    llvm::raw_string_ostream os(str);
+    os << "Type inference failed for op: ";
+    os << *op;
+    os.flush();
+
+    return TypeValue(str);
+  }
+
+  static TypeValue getInvalid(const TypeValue &lhs, const TypeValue &rhs) {
+    std::string str;
+    llvm::raw_string_ostream os(str);
+    os << "Incompatible types: ";
+    os << lhs.type << " and " << rhs.type;
+    os.flush();
+
+    return TypeValue(str);
+  }
+
+  static TypeValue join(const TypeValue &lhs, const TypeValue &rhs) {
+    if (!lhs.isInitialized())
+      return rhs;
+
+    if (!rhs.isInitialized())
+      return lhs;
+
+    if (!lhs.isValid())
+      return lhs;
+
+    if (!rhs.isValid())
+      return rhs;
+
+    mlir::Type lhsType = lhs.type;
+    mlir::Type rhsType = rhs.type;
+    if (lhsType == rhsType)
+      return lhs;
+
+    if (!lhsType || mlir::isa<hc::py_ir::UndefinedType>(lhsType))
+      return rhs;
+
+    if (!rhsType || mlir::isa<hc::py_ir::UndefinedType>(rhsType))
+      return lhs;
+
+    return getInvalid(lhs, rhs);
+  }
+
+  bool operator==(const TypeValue &rhs) const { return type == rhs.type; }
+
+  void print(llvm::raw_ostream &os) const {
+    if (!isValid()) {
+      os << "Invalid: " << errDesc;
+    } else if (!isInitialized()) {
+      os << "Uninitialized";
+    } else {
+      os << type;
+    }
+  }
+
+  mlir::Type getType() const { return type; }
+
+  bool isInitialized() const { return type != nullptr; }
+
+  bool isValid() const { return errDesc.empty(); }
+
+  mlir::StringRef getErrDesc() const {
+    assert(!isValid());
+    return errDesc;
+  }
+
+private:
+  mlir::Type type;
+  std::string errDesc;
+};
+
+struct TypeValueLattice : public mlir::dataflow::Lattice<TypeValue> {
+  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(TypeValueLattice)
+  using Lattice::Lattice;
+};
+
+class TypeAnalysis
+    : public mlir::dataflow::SparseForwardDataFlowAnalysis<TypeValueLattice> {
+public:
+  TypeAnalysis(mlir::DataFlowSolver &solver, TypingInterpreter &interp)
+      : SparseForwardDataFlowAnalysis(solver), interpreter(interp) {}
+
+  void visitOperation(mlir::Operation *op,
+                      llvm::ArrayRef<const TypeValueLattice *> operands,
+                      llvm::ArrayRef<TypeValueLattice *> results) override {
+    llvm::SmallVector<mlir::Type> argTypes;
+    for (auto arg : operands) {
+      auto &latticeVal = arg->getValue();
+      if (!latticeVal.isInitialized() || !latticeVal.isValid())
+        return;
+
+      argTypes.emplace_back(latticeVal.getType());
+    }
+
+    llvm::SmallVector<mlir::Type> result;
+    auto res = interpreter.run(op, argTypes, result);
+    if (mlir::failed(res)) {
+      auto errState = TypeValue::getInvalid(op);
+      for (auto resultLattice : results) {
+        auto changed = resultLattice->join(errState);
+        propagateIfChanged(resultLattice, changed);
+      }
+      return;
+    }
+
+    if (!*res)
+      return;
+
+    assert(result.size() == results.size());
+    for (auto &&[resultLattice, res] : llvm::zip_equal(results, result)) {
+      auto changed = resultLattice->join(TypeValue(res));
+      propagateIfChanged(resultLattice, changed);
+    }
+  }
+
+  void
+  visitNonControlFlowArguments(mlir::Operation *op,
+                               const mlir::RegionSuccessor &successor,
+                               mlir::ArrayRef<TypeValueLattice *> argLattices,
+                               unsigned firstIndex) override {
+    if (auto func = mlir::dyn_cast<hc::py_ir::PyFuncOp>(op)) {
+      for (auto &&[annotation, arg] :
+           llvm::zip_equal(func.getAnnotations(), func.getBlockArgs())) {
+        TypeValueLattice *annotationLattice = getLatticeElement(annotation);
+        TypeValue val = annotationLattice->getValue();
+        TypeValueLattice *argLattice = getLatticeElement(arg);
+        propagateIfChanged(argLattice, argLattice->join(val));
+      }
+    }
+
+    return SparseForwardDataFlowAnalysis::visitNonControlFlowArguments(
+        op, successor, argLattices, firstIndex);
+  }
+
+  void setToEntryState(TypeValueLattice *lattice) override {
+    propagateIfChanged(lattice, lattice->join(TypeValue{}));
+  }
+
+private:
+  TypingInterpreter &interpreter;
+};
+
+static mlir::Operation *getOp(mlir::Value val) {
+  if (auto defOp = val.getDefiningOp())
+    return defOp;
+
+  return val.getParentRegion()->getParentOp();
+}
+
 struct PyTypeInferencePass final
     : public hc::impl::PyTypeInferencePassBase<PyTypeInferencePass> {
 
@@ -190,64 +352,29 @@ struct PyTypeInferencePass final
     TypingInterpreter interp;
     interp.populate(rootOp);
 
-    llvm::SmallDenseMap<mlir::Value, mlir::Type> typemap;
-    auto getType = [&](mlir::Value val) -> mlir::Type {
-      auto it = typemap.find(val);
-      if (it == typemap.end())
+    mlir::DataFlowSolver solver;
+    solver.load<mlir::dataflow::DeadCodeAnalysis>();
+    solver.load<mlir::dataflow::SparseConstantPropagation>();
+    solver.load<TypeAnalysis>(interp);
+    if (mlir::failed(solver.initializeAndRun(rootOp)))
+      return signalPassFailure();
+
+    auto getType = [&](mlir::Value arg) -> mlir::Type {
+      auto *state = solver.lookupState<TypeValueLattice>(arg);
+      if (!state)
         return {};
 
-      return it->second;
+      auto &val = state->getValue();
+      if (!val.isValid()) {
+        getOp(arg)->emitError(val.getErrDesc());
+        signalPassFailure();
+        return {};
+      }
+      if (!val.isInitialized())
+        return {};
+
+      return val.getType();
     };
-
-    llvm::SmallVector<mlir::Type> argTypes;
-    llvm::SmallVector<mlir::Type> resTypes;
-
-    auto typingVisitor = [&](hc::py_ir::PyModuleOp op) -> mlir::WalkResult {
-      // TODO: Proper dataflow analysis
-      auto innerVisitor = [&](mlir::Operation *innerOp) -> mlir::WalkResult {
-        if (auto func = mlir::dyn_cast<hc::py_ir::PyFuncOp>(innerOp)) {
-          for (auto &&[arg, annotation] :
-               llvm::zip_equal(func.getBlockArgs(), func.getAnnotations())) {
-            auto type = getType(annotation);
-            if (!type)
-              continue;
-
-            typemap[arg] = type;
-          }
-        }
-        argTypes.clear();
-        for (mlir::Value arg : innerOp->getOperands()) {
-          mlir::Type type = getType(arg);
-          if (!type)
-            return mlir::WalkResult::advance();
-
-          argTypes.emplace_back(type);
-        }
-
-        resTypes.clear();
-        auto res = interp.run(innerOp, argTypes, resTypes);
-        if (mlir::failed(res)) {
-          innerOp->emitOpError("Type inference failed");
-          return mlir::WalkResult::interrupt();
-        }
-
-        if (*res) {
-          for (auto &&[type, res] :
-               llvm::zip_equal(resTypes, innerOp->getResults())) {
-            typemap[res] = type;
-          }
-        }
-
-        return mlir::WalkResult::advance();
-      };
-      if (op->walk<mlir::WalkOrder::PreOrder>(innerVisitor).wasInterrupted())
-        return mlir::WalkResult::interrupt();
-
-      return mlir::WalkResult::skip();
-    };
-
-    if (rootOp->walk<mlir::WalkOrder::PreOrder>(typingVisitor).wasInterrupted())
-      return signalPassFailure();
 
     updateTypes(rootOp, getType);
   }
