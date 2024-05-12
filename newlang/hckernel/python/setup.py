14f63495cde1900233c8a9175d3c421faad3fbb2
Ivan Butygin <ivan.butygin@gmail.com>
2024-05-12 13:59:29 +0200

    cleanup setup.py

--- newlang/hckernel/python/setup.py
+++ newlang/hckernel/python/setup.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 from setuptools import setup, find_packages
+import shutil
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 cmake_build_dir = os.path.join(root_dir, "build")
@@ -19,38 +20,54 @@ def get_env(env_name):
     return env_val
 
 
+def mkdir(path):
+    os.makedirs(path, exist_ok=True)
+
+
+def rmdir(path):
+    shutil.rmtree(path, ignore_errors=True)
+
+
+def clear_dir(path):
+    rmdir(path)
+    mkdir(path)
+
+
 LLVM_PATH = get_env("LLVM_PATH")
 LLVM_DIR = os.path.join(LLVM_PATH, "lib", "cmake", "llvm")
 MLIR_DIR = os.path.join(LLVM_PATH, "lib", "cmake", "mlir")
-LIT_DIR = os.path.join(root_dir, "..", "scripts", "runlit.py")
 
 cmake_cmd = [
-    "cmake",
     cmake_dir,
     "-GNinja",
     "-DCMAKE_INSTALL_PREFIX=" + install_dir,
     "-DCMAKE_BUILD_TYPE=Release",
     "-DLLVM_DIR=" + LLVM_DIR,
     "-DMLIR_DIR=" + MLIR_DIR,
-    "-DHC_ENABLE_TESTS=ON",
-    "-DLLVM_EXTERNAL_LIT=" + LIT_DIR,
+    "-DHC_ENABLE_TOOLS=OFF",
+    "-DHC_ENABLE_TESTS=OFF",
 ]
 
-subprocess.run(["mkdir", cmake_build_dir])
-subprocess.run(["mkdir", install_dir])
+mkdir(cmake_build_dir)
+clear_dir(install_dir)
 
-subprocess.check_call(
-    cmake_cmd, stderr=subprocess.STDOUT, shell=False, cwd=cmake_build_dir, env=env
-)
-subprocess.check_call(
-    ["cmake", "--build", ".", "--config", "Release"], cwd=cmake_build_dir, env=env
-)
-subprocess.check_call(
-    ["cmake", "--install", ".", "--config", "Release"], cwd=cmake_build_dir, env=env
-)
+
+def invoke_cmake(args):
+    subprocess.check_call(
+        ["cmake"] + args,
+        stderr=subprocess.STDOUT,
+        shell=False,
+        cwd=cmake_build_dir,
+        env=env,
+    )
+
+
+invoke_cmake(cmake_cmd)
+invoke_cmake(["--build", ".", "--config", "Release"])
+invoke_cmake(["--install", ".", "--config", "Release"])
 
 setup(
     name="hckernel",
-    version="1.0",
+    version="0.1",
     packages=find_packages(where=root_dir, include=["*", "hckernel", "hckernel.*"]),
 )
