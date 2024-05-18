d9959e4f0be875450e7003bc87368883f7364852
Ivan Butygin <ivan.butygin@gmail.com>
2024-05-18 12:46:23 +0200

    refac

--- newlang/hckernel/python/cpp/src/Dispatcher.cpp
+++ newlang/hckernel/python/cpp/src/Dispatcher.cpp
@@ -14,13 +14,13 @@ namespace py = pybind11;
   throw std::runtime_error(msg.str());
 }
 
-void Dispatcher::definePyClass(pybind11::module_ &m) {
+void Dispatcher::definePyClass(py::module_ &m) {
   py::class_<Dispatcher>(m, "Dispatcher")
       .def(py::init<py::capsule, py::object>())
       .def("__call__", &Dispatcher::call);
 }
 
-Dispatcher::Dispatcher(pybind11::capsule ctx, py::object getSrc)
+Dispatcher::Dispatcher(py::capsule ctx, py::object getSrc)
     : context(*ctx.get_pointer<Context>()), contextRef(std::move(ctx)),
       getSourceFunc(std::move(getSrc)) {}
 
